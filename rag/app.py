41adb57a33634082cc9e6eaddd29d4e10bae660f
Leonce Nshuti <leonce.nshuti@sonymusicpub.com>
2024-04-09 14:36:56 -0500

    add reference

--- rag/app.py
+++ rag/app.py
@@ -43,3 +43,5 @@ agent = AgentRunner(agent_worker)
 
 response = agent.chat("Tell me both the risk factors and tailwinds the United States versus China in Africa's mineral trade?")
 print(str(response))
+
+# Reference: LLamaIndex Example Implementation of the ANTHROPIC Agent. https://github.com/run-llama/llama_index/blob/main/docs/docs/examples/agent/anthropic_agent.ipynb
---
+++ rag/rag_readme.md
@@ -0,0 +1,8 @@
+
+
+
+
+
+##### Reference: 
+
+1. Logan Markewich. LLamaIndex Example Implementation of the ANTHROPIC Agent. https://github.com/run-llama/llama_index/blob/main/docs/docs/examples/agent/anthropic_agent.ipynb
